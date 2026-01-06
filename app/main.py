221ac9a39293292ef8967c413e7eddb1395bf90c
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-01-06 22:12:32 +0530

    feat: exposes the entrypoint for movies router

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from app.routes.Movies import router as movie_router
 
 app = FastAPI(title="movie recommender")
+
+app.include_router(movie_router)
--- app/routes/movies.py
+++ app/routes/movies.py
@@ -7,6 +7,7 @@ router = APIRouter()
 @router.post("/movies")
 def add_movie(movie:Movie):
     movie_dict = movie.model_dump()
+    
     return {
         "message": "Movie added successfully",
         "movie" : movie_dict
