21cc363caf28e663eb75351a3ee5320130dc2e09
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-01-06 22:10:30 +0530

    feat: adds movies route

--- app/routes/movies.py
+++ app/routes/movies.py
@@ -1,4 +1,13 @@
 from fastapi import APIRouter
+from app.models.Movie import Movie
+
 
 router = APIRouter()
 
+@router.post("/movies")
+def add_movie(movie:Movie):
+    movie_dict = movie.model_dump()
+    return {
+        "message": "Movie added successfully",
+        "movie" : movie_dict
+    }
